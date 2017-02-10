4e05f2b735a0b8b7e2bdad8b072cd7cf5653a297
AlexC <alex@cert.europa.eu>
2017-02-11 00:54:42 +0100

    Remove /export route
    The /ip-ranges endpoint should be used instead.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,30 +1,7 @@
-import csv
-from io import StringIO
-from flask import send_file, make_response
-from flask_login import login_required
+from flask import send_file
 from . import main
-from ..models import IpRange
-from ..api import ip_ranges
 
 
 @main.route('/')
 def index():
     return send_file('templates/index.html')
-
-
-@main.route('/export', defaults={'export_format': 'csv'})
-@main.route('/export/<string:export_format>')
-@login_required
-def export(export_format):
-    ranges = IpRange.query.filter(IpRange.deleted == 0).all()
-    if export_format == 'csv':
-        si = StringIO()
-        cw = csv.writer(si)
-        for ip_range in ranges:
-            cw.writerow([ip_range.ip_range])
-        out = make_response(si.getvalue())
-        out.headers["Content-Disposition"] = "attachment; filename=export.csv"
-        out.headers["Content-type"] = "text/csv"
-        return out
-    elif export_format == 'json':
-        return ip_ranges.get_ip_ranges()
