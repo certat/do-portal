1e07dd500e48d6a3bc94b9088fb698016529d080
Marcel Gruenauer <hanekomu@gmail.com>
2017-04-18 16:10:18 +0200

    tests/test_cp_users.py: test_return_certmaster_orgs() tests /organizations with demo data

--- tests/test_cp_users.py
+++ tests/test_cp_users.py
@@ -6,5 +6,10 @@ from app.models import User
 def test_return_certmaster_orgs(client):
     client.api_user = User.query.filter_by(name='certmaster').first()
     rv = client.get(url_for('cp.get_cp_organizations'))
-    print(rv.json)
-    assert_msg(rv, key='abbreviation')
+    assert_msg(rv, key='organizations')
+    got = list(rv.json.values())
+    got_abbreviations = [i['abbreviation'] for i in got[0]]
+    expect_abbreviations = ['cert', 'evn', 'evn-gas', 'evn-strom',
+                            'verbund', 'verbund-gas', 'verbund-strom',
+                            'verbund-strom-leitung']
+    assert set(got_abbreviations) == set(expect_abbreviations)
