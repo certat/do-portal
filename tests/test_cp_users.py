6e542831dccd64330af836c85a5b3bfe0ae00d22
Marcel Gruenauer <hanekomu@gmail.com>
2017-04-18 23:03:07 +0200

    tests/test_cp_users: test other users' organizations as well

--- tests/test_cp_users.py
+++ tests/test_cp_users.py
@@ -13,3 +13,21 @@ def test_return_certmaster_orgs(client):
                             'verbund', 'verbund-gas', 'verbund-strom',
                             'verbund-strom-leitung']
     assert set(got_abbreviations) == set(expect_abbreviations)
+
+
+def test_return_verbund_admin_orgs(client):
+    client.api_user = User.query.filter_by(name='Verbund Admin').first()
+    rv = client.get(url_for('cp.get_cp_organizations'))
+    assert_msg(rv, key='organizations')
+    got = list(rv.json.values())
+    got_abbreviations = [i['abbreviation'] for i in got[0]]
+    expect_abbreviations = ['verbund', 'verbund-gas', 'verbund-strom',
+                            'verbund-strom-leitung']
+    assert set(got_abbreviations) == set(expect_abbreviations)
+
+
+def test_return_verbund_ciso_orgs(client):
+    client.api_user = User.query.filter_by(name='Verbund CISO').first()
+    rv = client.get(url_for('cp.get_cp_organizations'))
+    assert rv.status_code == 200
+    assert len(rv.json['organizations']) == 0, 'no organizations'
