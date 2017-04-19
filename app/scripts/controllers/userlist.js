3f40c068c795cc1c7f2cc478d275e2d0a7b10bb4
David Schmidt <mail@davidschmidt.at>
2017-04-19 16:10:30 +0200

    WIP create new user+membership

--- app/scripts/controllers/userlist.js
+++ app/scripts/controllers/userlist.js
@@ -9,24 +9,36 @@
  */
 
 angular.module('cpApp')
-  .controller('UserListCtrl', function ($scope, $filter, $uibModal, User, Auth, GridData, notifications) {
+  .controller('UserListCtrl', function ($scope, $filter, $uibModal, User, Membership, Auth, GridData, notifications) {
+
+    function _update_users() {
+      User.query_list().$promise.then(function(resp){
+        $scope.users = resp.users;
+      }, function(err){
+          console.log(err);
+        notifications.showError(err.data.message);
+      });
+    }
+    _update_users();
 
     $scope.show_form = false;
 
     $scope.create_user = function(u){
       User.create({}, u, function(resp){
-        $scope.show_form = false;
-        notifications.showSuccess(resp);
+
+        var m = angular.merge({}, resp.membership, $scope.membership);
+
+        Membership.update({'id':m.id}, m, function(resp) {
+          $scope.show_form = false;
+          _update_users();
+          notifications.showSuccess(resp);
+        }, function(error){
+          notifications.showError(error.data);
+        });
+
       }, function(error){
         notifications.showError(error.data);
       });
     };
-
-    User.query_list().$promise.then(function(resp){
-      $scope.users = resp.users;
-    }, function(err){
-        console.log(err);
-      notifications.showError(err.data.message);
-    });
 
   });
