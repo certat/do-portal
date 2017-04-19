62365b1734637f95b8e0e43d1d063e94459cbec6
David Schmidt <mail@davidschmidt.at>
2017-04-19 11:12:49 +0200

    chain promises

--- app/scripts/controllers/useredit.js
+++ app/scripts/controllers/useredit.js
@@ -8,15 +8,52 @@
  * Controller of the cpApp
  */
 angular.module('cpApp')
-  .controller('UsereditCtrl', function ($scope, $filter, $uibModal, User, Auth, GridData, notifications, $stateParams) {
+  .controller('UsereditCtrl', function ($scope, $filter, $uibModal, User, Auth, GridData, notifications, $stateParams, $q) {
 
-    User.query({'id': $stateParams.id}).$promise.then(function(resp){
-      var user = resp;
-      _setup_memberships(user);
-      $scope.user = user;
-    }, function(err){
-      notifications.showError(err.data.message);
-    });
+    var loadUser = function() {
+      return User.query({'id': $stateParams.id}).$promise
+                .then(function(resp){
+                    return resp;
+                  }, function(err){
+                    notifications.showError(err.data.message);
+                  });
+    };
+
+    var loadRoles = function(){
+      return User.roles().$promise
+                .then(function(resp){
+                    return resp.membership_roles;
+                  }, function(err){
+                    notifications.showError(err.data.message);
+                  });
+    };
+
+    var loadMemberships = function(){
+      return User.memberships().$promise
+                .then(function(resp){
+                    return resp.organization_memberships;
+                  }, function(err){
+                    notifications.showError(err.data.message);
+                  });
+    };
+
+    var loadParallel = function() {
+        return $q.all([ loadUser(), loadRoles(), loadMemberships() ])
+            .then( function( result ) {
+              $scope.user = result.shift();
+              $scope.roles = result.shift();
+              $scope.memberships = result.shift()
+                    .filter(function(val){return val.user_id === $scope.user.id});
+              console.log($scope.memberships);
+//+        $scope.memberships = [
+//+            { org: { id: 1, name: "org1" }, role: { id: 1, name: "role1" } },
+//+            { org: { id: 2, name: "org2" }, role: { id: 2, name: "role2" } },
+//+            { org: { id: 3, name: "org3" }, role: { id: 3, name: "role3" } },
+//+        ];
+            });
+    };
+
+    loadParallel();
 
     $scope.save = function(u){
       User.update({'id':u.id}, u, function(resp){
@@ -25,28 +62,5 @@ angular.module('cpApp')
         notifications.showError(error.data);
       });
     };
-
-    function _setup_memberships(u) {
-
-      /*
-      User.memberships().$promise.then(function(resp){
-        $scope.memberships = resp.organization_memberships.filter(function(val){return val.user_id === u.id});
-      }, function(err){
-        notifications.showError(err.data.message);
-      });
-
-      User.roles().$promise.then(function(resp){
-        $scope.roles = resp.membership_roles;
-      }, function(err){
-        notifications.showError(err.data.message);
-      });
-      */
-
-      $scope.memberships = [
-          { org: { id: 1, name: "org1" }, role: { id: 1, name: "role1" } },
-          { org: { id: 2, name: "org2" }, role: { id: 2, name: "role2" } },
-          { org: { id: 3, name: "org3" }, role: { id: 3, name: "role3" } },
-      ];
-    }
 
   });
