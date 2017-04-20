b6846d77681320b6a2668a320e2d0ca3b0c6bae4
Mark Hofstetter <mark@univie.ac.at>
2017-04-20 11:56:08 +0200

    more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,5 @@
-from app.models import User, Organization
+from app.models import User, Organization, MembershipRole, \
+    OrganizationMembership
 from app import db
 # from .conftest import assert_msg
 # from app.fixtures import testfixture
@@ -19,7 +20,7 @@ def test_user_memberships():
 
 def test_get_users():
     u = User.query.filter_by(name="certmaster").first()
-    c = 0
+    # c = 0
     # for user in u.get_users():
     #    c += 1
     assert len(u.get_users()) == 7, 'find all subordinate users - once'
@@ -34,21 +35,32 @@ def test_create_user():
 
     + create new user
     + try to
-
     """
+    username = 'Testuser under Verbund Admin'
 
     admin = User.query.filter_by(name="Verbund Admin").first()
     assert len(admin.user_memberships) == 3
     org = admin.get_organizations().first()
 
+    c = len(admin.get_users())
+    assert c == 3, 'Verbung Admin has 3 users'
     certorg = Organization.query.filter_by(abbreviation='cert').first()
 
-    username = 'Testuser under Verbund Admin'
     newuser = User(name=username)
     newuser.password = 'bla'
     db.session.add(newuser)
     db.session.commit()
     assert newuser.id > 0
     assert admin.may_handle_organization(certorg) is False, \
-       'verbund admin may not handle cert org'
+        'verbund admin may not handle cert org'
     assert admin.may_handle_organization(org) is True
+    role = MembershipRole.query.filter_by(name='CISO').first()
+    oxu = OrganizationMembership(
+        phone='4711',
+        organization=org,
+        user=newuser,
+        membership_role=role,
+    )
+    db.session.commit()
+    assert oxu.id > 0, 'OrganizationMembership written'
+    assert len(admin.get_users()), 'Verbund Admin now has 4 users'
