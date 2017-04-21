9c72a5b4991f7a6038801f664830fa832ecef8b7
Marcel Gruenauer <hanekomu@gmail.com>
2017-04-21 12:05:03 +0200

    tests/test_cp_organizations.py: use test data for some tests

--- tests/test_cp_organizations.py
+++ tests/test_cp_organizations.py
@@ -1,5 +1,6 @@
 from flask import url_for
 from .conftest import assert_msg
+from app.models import User
 
 
 def test_create_org(client):
@@ -36,18 +37,36 @@ def test_update_org(client):
 
 
 def test_return_orgs(client):
+    client.api_user = find_user_by_name('Verbund Admin')
     rv = client.get(url_for('cp.get_cp_organizations'))
     assert_msg(rv, key='organizations')
+    got = list(rv.json.values())
+    got_ids = [i['id'] for i in got[0]]
 
-    rv = client.get(url_for('cp.get_cp_organization',
-                    org_id=client.test_user.organization_id))
-    assert_msg(rv, key='abbreviation')
+    for org_id in got_ids:
+        rv = client.get(url_for('cp.get_cp_organization',
+                        org_id=org_id))
+        assert_msg(rv, key='abbreviation')
 
 
-def test_del_org(client):
-    rv = client.delete(url_for('cp.delete_cp_organization',
-                       org_id=client.test_user.organization_id))
-    assert_msg(rv, value='Organization deleted')
+def test_delete_org(client):
+    client.api_user = find_user_by_name('Verbund Admin')
+    rv = client.get(url_for('cp.get_cp_organizations'))
+    assert_msg(rv, key='organizations')
+    got = list(rv.json.values())
+    got_ids = [i['id'] for i in got[0]]
 
+    for org_id in got_ids:
+        rv = client.delete(url_for('cp.delete_cp_organization',
+                           org_id=org_id))
+        assert_msg(rv, value='Organization deleted')
+
+
+def test_delete_nonexistent_org(client):
+    client.api_user = find_user_by_name('Verbund Admin')
     rv = client.delete(url_for('cp.delete_cp_organization', org_id=666))
     assert rv.status_code == 404
+
+
+def find_user_by_name(name):
+    return User.query.filter_by(name=name).first()
