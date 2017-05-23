8db9d2ab0d854498c6680ce207193f1737ad856c
David Schmidt <mail@davidschmidt.at>
2017-05-23 16:00:49 +0200

    hierarchical presentation of orgs

--- app/scripts/controllers/organizationlist.js
+++ app/scripts/controllers/organizationlist.js
@@ -10,10 +10,31 @@
 angular.module('cpApp')
   .controller('OrganizationListCtrl', function ($scope, $filter, $uibModal, Organization, Auth, GridData, notifications) {
     Organization.query_list().$promise.then(function(resp){
-      $scope.orgs = resp.organizations;
+      var orgs = resp.organizations;
+      set_offset(orgs);
+      $scope.orgs = orgs;
     }, function(err){
         console.log(err);
       notifications.showError(err.data.message);
     });
+
+    function set_offset(orgs, parentid, offset) {
+      if (!parentid) {
+        orgs.forEach(function(o) {
+          if (!o.parent_org_id) {
+            o.offset = 0;
+            set_offset(orgs, o.id, 1);
+          }
+        });
+      }
+      else {
+        orgs.forEach(function(o) {
+          if (o.parent_org_id === parentid) {
+            o.offset = offset;
+            set_offset(orgs, o.id, offset+1);
+          }
+        });
+      }
+    }
 
   });
