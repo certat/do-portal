a7d1d6535b79c88adc6e76481e4f8ed8b40af223
David Schmidt <mail@davidschmidt.at>
2017-06-06 11:00:43 +0200

    show organizations user belongs to

--- app/scripts/controllers/userlist.js
+++ app/scripts/controllers/userlist.js
@@ -9,13 +9,64 @@
  */
 
 angular.module('cpApp')
-  .controller('UserListCtrl', function ($scope, $filter, $uibModal, User, Auth, GridData, notifications) {
+  .controller('UserListCtrl', function ($scope, $filter, $uibModal, User, Membership, Organization, Auth, GridData, notifications, $q) {
 
-    User.query_list().$promise.then(function(resp){
-      $scope.users = resp.users;
-    }, function(err){
-        console.log(err);
-      notifications.showError(err.data.message);
-    });
+    var loadUsers = function() {
+      return User.query_list().$promise
+                .then(function(resp){
+                    return resp.users;
+                  }, function(err){
+                    notifications.showError(err.data.message);
+                  });
+    };
+
+    var loadMemberships = function(){
+      return Membership.query().$promise
+                .then(function(resp){
+                    return resp.organization_memberships;
+                  }, function(err){
+                    notifications.showError(err.data.message);
+                  });
+    };
+
+    var loadOrganizations = function(){
+      return Organization.query_list().$promise
+                .then(function(resp){
+                    return resp.organizations;
+                  }, function(err){
+                    notifications.showError(err.data.message);
+                  });
+    };
+
+    function _array2hash(arr) {
+        var hash = {};
+        arr.forEach(function(i) { hash[i.id] = i; });
+        return hash;
+    }
+    var loadParallel = function() {
+        return $q.all([ loadUsers(), loadMemberships(), loadOrganizations() ])
+            .then( function( result ) {
+              $scope.users         = _array2hash(result.shift());
+              $scope.memberships   = result.shift();
+              $scope.organizations = _array2hash(result.shift());
+
+              $scope.memberships.forEach(function(m) {
+                var u = $scope.users[m.user_id];
+
+                if (!u.hasOwnProperty('organizations')) {
+                  u.organizations = [];
+                }
+
+                u.organizations.push($scope.organizations[m.organization_id].full_name);
+              });
+
+              Object.keys($scope.users).forEach(function(id){
+                $scope.users[id].organizations = $scope.users[id].organizations.join(', ');
+              });
+            }
+        );
+    };
+
+    loadParallel();
 
   });
