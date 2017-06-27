9aebc69df23ef88251e9f16bb03a5b3feeee20d8
David Schmidt <mail@davidschmidt.at>
2017-06-27 11:09:43 +0200

    fix tree building algorithm

--- app/scripts/controllers/organizationlist.js
+++ app/scripts/controllers/organizationlist.js
@@ -10,28 +10,29 @@
 angular.module('cpApp')
   .controller('OrganizationListCtrl', function ($scope, $filter, $uibModal, Organization, Auth, GridData, notifications) {
     Organization.query_list().$promise.then(function(resp){
-      var orgs = resp.organizations;
-      set_offset(orgs);
+      var orgs = [];
+      make_tree(orgs, resp.organizations)
       $scope.orgs = orgs;
     }, function(err){
-        console.log(err);
       notifications.showError(err.data.message);
     });
 
-    function set_offset(orgs, parentid, offset) {
+    function make_tree(output_orgs, input_orgs, parentid, offset) {
       if (!parentid) {
-        orgs.forEach(function(o) {
+        input_orgs.forEach(function(o) {
           if (!o.parent_org_id) {
             o.offset = 0;
-            set_offset(orgs, o.id, 1);
+            output_orgs.push(o);
+            make_tree(output_orgs, input_orgs, o.id, 1);
           }
         });
       }
       else {
-        orgs.forEach(function(o) {
+        input_orgs.forEach(function(o) {
           if (o.parent_org_id === parentid) {
             o.offset = offset;
-            set_offset(orgs, o.id, offset+1);
+            output_orgs.push(o);
+            make_tree(output_orgs, input_orgs, o.id, offset+1);
           }
         });
       }
