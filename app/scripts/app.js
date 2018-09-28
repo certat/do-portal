e239b233c176b4f517a35a2ffebdc1aa13fb3e25
David Schmidt <mail@davidschmidt.at>
2018-09-28 09:59:04 +0200

    rm /user-export

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -100,19 +100,6 @@ angular
           }
         }
       })
-      .state('user_export', {
-        url: '/user_export',
-        views: {
-          header: {
-            templateUrl: 'views/header.html',
-            controller: 'HeaderCtrl'
-          },
-          content: {
-            templateUrl: 'views/user-export.html',
-            controller: 'UserListCtrl'
-          }
-        }
-      })
       .state('user_list', {
         url: '/user_list',
         views: {
