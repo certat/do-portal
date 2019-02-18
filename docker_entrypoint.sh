1210ee426753228b758de0500a7fe4e401a8db30
David Schmidt <mail@davidschmidt.at>
2019-02-18 11:10:00 +0100

    use new migrate methods to bootstrap DB

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -23,17 +23,21 @@ if PGPASSWORD=do_portal psql -U do_portal -h cert_db -d do_portal -lqt | cut -d 
   PGPASSWORD=do_portal dropdb -U do_portal -h cert_db do_portal;
   echo '### createdb'
   PGPASSWORD=do_portal createdb -U do_portal -h cert_db -O do_portal do_portal;
-  mv misc/migrations misc/tmp-migrations
+  mv misc/migrations misc/tmp-migrations # TODO remove hack
   echo '### init'
   python3 manage.py db init;
   echo '### migrate'
   python3 manage.py db migrate;
   echo '### upgrade'
   python3 manage.py db upgrade;
+  echo '### initializedb'
+  python3 manage.py initializedb;
+  echo '### insertmasteruser'
+  python3 manage.py insertmasteruser;
   echo '### addyaml'
   python3 demodata.py addyaml
-  rm -rf misc/migrations
-  mv misc/tmp-migrations misc/migrations
+  rm -rf misc/migrations # TODO remove hack
+  mv misc/tmp-migrations misc/migrations # TODO remove hack
 
   echo '### init RIPE/FODY'
   #PGPASSWORD=do_portal psql -h db -U do_portal -c "CREATE SCHEMA fody";
