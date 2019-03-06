f957a843ffbeb9a64ca830c167ed38095b03dd82
Mark Hofstetter <mark@univie.ac.at>
2019-03-06 14:51:13 +0100

    added example proxy for grafana

---
+++ backend/app/cp/grafana.py
@@ -0,0 +1,29 @@
+from flask import g, request, abort
+from app.core import ApiResponse
+from app import db
+from . import cp
+from flask import Response
+import requests
+
+
+@cp.route('/grafana', methods=['GET'])
+def get_grafana():
+    request_headers = {key: value for (key, value) in request.headers  if key != 'Host'}
+    request_headers['X_GRAFANA_REMOTE_USER'] = 'dope'
+    resp = requests.request(
+        method=request.method,
+        url='http://localhost:3005/d/QA7iWe9iz/teshboard?orgId=1',  # request.url.replace(request.host_url, 'orf.at'),
+        headers=request_headers,
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+
+    return response
+
+
