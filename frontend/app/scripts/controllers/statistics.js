4f7cc87c0c826faefd21697baac87bc23c91b511
David Schmidt <mail@davidschmidt.at>
2019-03-12 13:31:10 +0100

    fix js style warning: quote marks

--- frontend/app/scripts/controllers/statistics.js
+++ frontend/app/scripts/controllers/statistics.js
@@ -11,7 +11,7 @@ angular.module('cpApp')
   .controller('StatisticsCtrl', function ($scope, $stateParams, $sce, config) {
       var url = config.apiConfig.webServiceUrl + '/statistics';
       if ($stateParams.orgid) {
-          url = url + "?orgid=" + $stateParams.orgid;
+          url = url + '?orgid=' + $stateParams.orgid;
       }
       $scope.statistics_url = $sce.trustAsResourceUrl(url);
 });
