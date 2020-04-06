8d3844d83674bc0c356a0d26e2ebdc48ae5e34ee
David Schmidt <mail@davidschmidt.at>
2020-04-06 16:45:16 +0200

    fix: always create configs

--- frontend/docker_entrypoint.sh
+++ frontend/docker_entrypoint.sh
@@ -10,15 +10,11 @@ npm install
 
 PATH=$(npm bin):$PATH bower install
 
-if [ ! -f config/envs/devel.json ]; then
-    echo 'create config/envs/devel.json.'
-  cp config/envs/docker.json config/envs/devel.json
-fi
+echo 'create config/envs/devel.json.'
+cp config/envs/docker.json config/envs/devel.json
 
-if [ ! -f config/envs/production.json ]; then
-    echo 'create config/envs/production.json.'
-    cp config/envs/docker.json config/envs/production.json
-fi
+echo 'create config/envs/production.json.'
+cp config/envs/docker.json config/envs/production.json
 
 if [ ! -f secret.json ]; then
     echo 'create secret.json.'
